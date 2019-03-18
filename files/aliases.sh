8a28d8934494e23012f993cf4b6892226e2b0e53
Jakub Petriska <jakubpetriska@gmail.com>
2019-03-18 14:28:49 +0100

    Add git alias for last branch commit

--- files/aliases.sh
+++ files/aliases.sh
@@ -6,3 +6,11 @@ alias gc='git commit'
 alias gl='git log --oneline | head -n'
 alias gf='git fetch'
 alias la='ls -la'
+
+print_last_branch_commit () {
+  if git checkout $1 -q; then
+    gl 1;
+    git checkout - -q;
+  fi
+}
+alias glc=print_last_branch_commit
